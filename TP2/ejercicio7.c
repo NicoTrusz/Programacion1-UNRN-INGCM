20e113f4599bd685b1a4bb65b1be77884bc052f6
NicoTrusz <nicolastrusz@gmail.com>
2025-08-23 17:11:47 -0300

    ejercicio 7

--- TP2/ejercicio7.c
+++ TP2/ejercicio7.c
@@ -3,3 +3,41 @@
  * Autor: Nicolás Trusz
  * Usuario GitHub: nicolastrusz
  */
+#include<stdio.h>
+int multiplicacion(int numero1, int numero2)
+{
+    int resultado = 0;
+    int i;
+    for(i =0; i <numero2; i++)
+    {
+        resultado += numero1;
+    }
+    return resultado;
+}
+
+int potenciacion_lenta(int base, int exponente)
+{
+    int resultado = 1;
+    int i;
+    for (i=0; i < exponente; i++)
+    {
+        resultado = multiplicacion(resultado,base);
+    }
+    return resultado;
+}
+
+int main ()
+{
+ int base = 0;
+ int exponente = 0;
+ int resultado;
+ // entrada
+ printf("ingrese el numero que quiere elevar: ");
+ scanf("%d",&base);
+ printf("ingrese el exponenete: ");
+ scanf("%d",&exponente);
+resultado = potenciacion_lenta(base, exponente);
+//salida
+printf("%d elevado a %d es: %d",base,exponente,resultado);
+ return 0;
+}
