837bc60ffc32f227d02dd211c4d35e8ebd01ceb7
NicoTrusz <nicolastrusz@gmail.com>
2025-09-05 21:30:02 -0300

    actualizacion T1

--- TP1/ejercicio13.c
+++ TP1/ejercicio13.c
@@ -1,7 +1,32 @@
 /* 
  * Ejercicio 13
  * Autor: Nicolás Trusz
- * Usuario GitHub: nicolastrusz
+ * Usuario GitHub: nicotrusz
  */
 #include<stdio.h>
 #include<stdlib.h>
+
+void mostrar_triangulo_pascal (int n)
+{
+    int i;
+    int j;
+    for (i=0; i< n; i++)
+    {
+        long val = 1;
+        for (j=0; j<= i; j++)
+        {
+            printf("%ld",val);
+            val = val* (i-j)/(j+1);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int filas;
+    printf("Ingrese el numero de filas: ");
+    scanf("%d",&filas);
+    mostrar_triangulo_pascal(filas);
+    return 0;
+}
--- TP1/ejercicio14.c
+++ TP1/ejercicio14.c
@@ -5,3 +5,40 @@
  */
 #include<stdio.h>
 #include<stdlib.h>
+
+int suma_divisores_propios(int numero)
+{
+    int suma = 0;
+    for (int i =1; i < numero; i++)
+    {
+        if (numero % i == 0)
+        {
+            suma += i;
+        }
+    }
+    return suma;
+}
+
+void verificar_amistad (int a, int b)
+{
+    int sumaA = suma_divisores_propios(a);
+    int sumaB = suma_divisores_propios(b);
+    if (sumaA == b && sumaB == a)
+    {
+        printf("%d y %d son numeros amigos \n", a,b);
+    }
+    else
+    {
+        printf("%d y %d no son numeros amigos \n", a,b);
+    }
+}
+
+int main ()
+{
+    int numero1 ;
+    int numero2 ;
+    printf("Ingrese dos numeros positivos: ");
+    scanf("%d %d", &numero1,&numero2);
+    verificar_amistad(numero1 , numero2);
+    return 0;
+}
