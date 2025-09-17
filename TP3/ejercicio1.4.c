e397a6dbe35966816483b9a793194a92f3d1dc18
NicoTrusz <nicolastrusz@gmail.com>
2025-09-17 15:39:03 -0300

    Actualizacion

--- TP3/ejercicio1.4.c
+++ TP3/ejercicio1.4.c
@@ -15,29 +15,43 @@
  * @param capacidad Tamaño físico máximo del arreglo destino.
  * @param insertar Cadena a insertar.
  * @param posicion Posición en la que se insertará la cadena.
- * @returns Largo final de la cadena destino o código de error negativo.
+ * @param bandera Largo final de la cadena destino o código de error negativo.
  */
-int insertar_seguro(char destino[], size_t capacidad, const char insertar[], size_t posicion) {
+int insertar_seguro(char destino[], size_t capacidad, const char insertar[], size_t posicion) 
+{
     size_t largo_destino = 0;
     size_t largo_insertar = 0;
+    int bandera = 0;
 
     // Calcular largo de destino
     while (largo_destino < capacidad && destino[largo_destino] != '\0') {
         largo_destino++;
     }
-    if (largo_destino == capacidad) return CADENA_SIN_TERMINADOR;
+    if (largo_destino == capacidad)
+    {
+      bandera =  CADENA_SIN_TERMINADOR;  
+    } 
 
     // Calcular largo de insertar
     while (largo_insertar < capacidad && insertar[largo_insertar] != '\0') {
         largo_insertar++;
     }
-    if (largo_insertar == capacidad) return CADENA_SIN_TERMINADOR;
+    if (largo_insertar == capacidad)
+    {
+        bandera = CADENA_SIN_TERMINADOR;
+    } 
 
     // Verificar posición válida
-    if (posicion > largo_destino) return ERROR_CAPACIDAD_INSUFICIENTE;
+    if (posicion > largo_destino)
+    {
+      bandera = ERROR_CAPACIDAD_INSUFICIENTE;  
+    } 
 
     // Verificar capacidad suficiente
-    if (largo_destino + largo_insertar >= capacidad) return ERROR_CAPACIDAD_INSUFICIENTE;
+    if (largo_destino + largo_insertar >= capacidad)
+    {
+        bandera = ERROR_CAPACIDAD_INSUFICIENTE;
+    }
 
     // Desplazar caracteres en destino para hacer espacio
     for (size_t i = largo_destino; i >= posicion; i--) {
@@ -49,8 +63,9 @@ int insertar_seguro(char destino[], size_t capacidad, const char insertar[], siz
     for (size_t i = 0; i < largo_insertar; i++) {
         destino[posicion + i] = insertar[i];
     }
+    bandera = largo_destino + largo_insertar; 
 
-    return largo_destino + largo_insertar;
+    return bandera;
 }
 
 #include <stdio.h>
