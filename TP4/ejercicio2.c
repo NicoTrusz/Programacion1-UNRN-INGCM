7bd9cf3e43ec22a1ad08a3603494a4cf222652cb
NicoTrusz <nicolastrusz@gmail.com>
2025-09-21 11:07:46 -0300

    TP 4

--- TP4/ejercicio2.c
+++ TP4/ejercicio2.c
@@ -3,3 +3,54 @@
  * Autor: Nicolás Trusz
  * Usuario GitHub: nicotrusz
  */
+#include <stdio.h>
+
+#define FILAS 3
+#define COLUMNAS 3
+
+void multiplicarMatrices(int A[FILAS][COLUMNAS], int B[FILAS][COLUMNAS], int C[FILAS][COLUMNAS]) {
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            C[i][j] = 0; // Inicializar acumulador
+            for (int k = 0; k < COLUMNAS; k++) {
+                C[i][j] = A[i][k] * B[k][j] + C[i][j];
+            }
+        }
+    }
+}
+
+void imprimirMatriz(int M[FILAS][COLUMNAS]) {
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            printf("%d", M[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int A[FILAS][COLUMNAS] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int B[FILAS][COLUMNAS] = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
+
+    int C[FILAS][COLUMNAS];
+
+    multiplicarMatrices(A, B, C);
+
+    printf("Matriz A:\n");
+    imprimirMatriz(A);
+    printf("Matriz B:\n");
+    imprimirMatriz(B);
+    printf("Matriz C (A x B):\n");
+    imprimirMatriz(C);
+
+    return 0;
+}
--- TP4/ejercicio3.c
+++ TP4/ejercicio3.c
@@ -3,3 +3,46 @@
  * Autor: Nicolás Trusz
  * Usuario GitHub: nicotrusz
  */
+
+ #include<stdio.h>
+
+ #define M_CUADRADA 3
+
+ void matrisz_traspuesta(int original[M_CUADRADA][M_CUADRADA], int traspuesta[M_CUADRADA][M_CUADRADA])
+ {
+    for (int i =0; i< M_CUADRADA; i++)
+    {
+        for(int j=0; j<M_CUADRADA; j++)
+        {
+            traspuesta[i][j]=original[j][i];
+        }
+    }
+ }
+
+ void imprimir_matrisz(int matrisz[M_CUADRADA][M_CUADRADA])
+ {
+    for (int i =0; i< M_CUADRADA; i++)
+    {
+        for(int j=0; j<M_CUADRADA; j++)
+        {
+            printf("%d",matrisz[i][j]);
+        }
+        printf("\n");
+    }
+ }
+
+ int main ()
+ {
+    int A[M_CUADRADA][M_CUADRADA]={{1,2,3},{4,5,6},{7,8,9}};
+    int T[M_CUADRADA][M_CUADRADA];
+
+    printf("Matriz Original: \n");
+    imprimir_matrisz(A);
+
+    matrisz_traspuesta(A,T);
+
+    printf("\nMatrisz Transpuesta:\n");
+    imprimir_matrisz(T);
+
+    return 0;
+ }
