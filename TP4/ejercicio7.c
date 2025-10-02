4be324dd19511f611fbc9eda5b0ff24c753ad6ba
NicoTrusz <nicolastrusz@gmail.com>
2025-10-02 00:01:48 -0300

    rot+90 y rot-90

--- TP4/ejercicio7.c
+++ TP4/ejercicio7.c
@@ -11,11 +11,20 @@ void rotar_90 (int A[N][N],int R[N][N])
     for(int i =0; i<N;i++)
         for(int j=0; j<N; j++)
         {
-             R[i][j] = A[N-1-j][i];
+            R[i][j] = A[N-1-j][i];
+        }
+}
+// rotacion 90 a izq del parcial era menos complicado de lo que me plantie en el parcial 
+void rotar_90_izq (int A[N][N],int R[N][N])
+{
+    for(int i =0; i<N;i++)
+        for(int j=0; j<N; j++)
+        {
+            R[i][j] = A[j][N-1-i];
         }
 }
 
- void imprimir_matriz(int M[N][N])
+void imprimir_matriz(int M[N][N])
 {
     for(int i=0; i<N; i++)
     {
@@ -35,6 +44,9 @@ int main()
     imprimir_matriz(A);
     rotar_90(A,R);
     printf("\nMatriz rotada 90 \n");
+    imprimir_matriz(R);
+    printf("\nMatriz rotada 90 izq \n");
+    rotar_90_izq(A,R);
     imprimir_matriz(R);
 
     return 0;
