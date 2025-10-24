42e95d5c1d877db0c79d29423f4e5b46ee4b5a7a
NicoTrusz <nicolastrusz@gmail.com>
2025-10-24 00:09:04 -0300

    MD

--- TP7/ejercicio2.c
+++ TP7/ejercicio2.c
@@ -3,3 +3,34 @@
  * Autor: Nicolás Trusz
  * Usuario GitHub: nicotrusz
  */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+ int variable_Global = 10;
+ void mostrarDirecciones()
+ {
+    int variable_Local = 20;
+
+    static int variable_Estatica = 30;
+    int *variable_Dinamica = (int *)malloc(sizeof(int));
+    if (variable_Dinamica == NULL)
+    {
+        printf("Error al asignar memoria dinamica.\n");
+        free(variable_Dinamica);
+        exit(1);
+    }
+    *variable_Dinamica = 40;
+    printf("Direccion de variable global     : %p (segmento de datos)\n", (void *)&variable_Global);
+    printf("Direccion de variable local      : %p (segmento stack)\n", (void *)&variable_Local);
+    printf("Direccion de variable estatica   : %p (segmento de datos)\n", (void *)&variable_Estatica);
+    printf("Direccion de variable dinamica   : %p (segmento heap)\n", (void *)variable_Dinamica);
+
+    free(variable_Dinamica);
+ }
+
+ int main()
+ {
+    mostrarDirecciones();
+    return 0;
+ }
--- TP7/ejercicio3.c
+++ TP7/ejercicio3.c
@@ -3,3 +3,39 @@
  * Autor: Nicolás Trusz
  * Usuario GitHub: nicotrusz
  */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char *copiar_cadena(const char *src)
+{   
+    char *bandera = NULL;
+
+    if (src != NULL) {
+        char *copia_src = malloc(strlen(src) + 1);
+        if (copia_src != NULL) {
+            strcpy(copia_src, src);
+            bandera = copia_src;
+        } else {
+            printf("Error: no se pudo asignar memoria\n");
+        }
+    }
+    return bandera;
+}
+
+int main ()
+{
+    char entrada [100];
+
+    printf("Ingrese una cadena: ");
+    fgets(entrada, sizeof(entrada), stdin);
+
+    entrada[strcspn(entrada, "\n")] = '\0';
+
+    char *copia = copiar_cadena(entrada);
+    if (copia != NULL)
+    {
+        printf("Cadena copiada: %s\n", copia);
+        free(copia);
+    }
+}
